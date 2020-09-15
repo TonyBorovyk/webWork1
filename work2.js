1d79e9646b88c9f4b7725f907656020107be6211
Anton Borovyk <54375146+TonyBorovyk@users.noreply.github.com>
2020-09-15 14:46:11 +0300

    Update work2.js

--- work2.js
+++ work2.js
@@ -1,5 +1,5 @@
   
 const butt = document.getElementById('butt'),
     input = document.getElementById('in'),
-    target = document.getElementById('inf')
+    inf = document.getElementById('inf')
     button.addEventListener("click", e => target.textContent = input.value)
